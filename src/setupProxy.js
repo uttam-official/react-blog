c43b1b4124a78884e60ccc2f022f031473fda84d
uttam-official <uttamkhuntia1998@gmail.com>
2021-10-25 17:09:06 +0530

    React Blog

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://blog-api-uttam.herokuapp.com/api/',
+      changeOrigin: true,
+    })
+  );
+};
